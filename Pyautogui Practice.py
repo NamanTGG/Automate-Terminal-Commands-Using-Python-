d5cc47be4f43e8f41e5e88b8a17faf4ddb35853b
NamanTGG <67754467+NamanTGG@users.noreply.github.com>
2020-09-28 15:20:15 +0530

    Add files via upload

---
+++ Pyautogui Practice.py
@@ -0,0 +1,49 @@
+# In This Program There Are Different Sections That Run Separately And You Can Run It By
+# Commenting In Bulk Which Is Taught In The Tutorial Or You Can Just Put The Separate Sections In Different Files And Then Run It
+import pyautogui
+#import time if you want a delay
+    #pip
+pyautogui.keyDown('p')
+pyautogui.keyDown('i')
+pyautogui.keyDown('p')
+pyautogui.keyDown('3') # You might not need this line depending on what Python Version You Are Using
+    #install
+pyautogui.keyDown(' ')
+pyautogui.keyDown('i')
+pyautogui.keyDown('n')
+pyautogui.keyDown('s')
+pyautogui.keyDown('t')
+pyautogui.keyDown('a')
+pyautogui.keyDown('l')
+pyautogui.keyDown('l')
+pyautogui.keyDown(' ')
+    # Module Name - In This Case : pyttsx3 - You Can Change It To Any Different Module/Package For Example Flask , tensorflow
+pyautogui.keyDown('p')
+pyautogui.keyDown('y')
+pyautogui.keyDown('t')
+pyautogui.keyDown('t')
+pyautogui.keyDown('s')
+pyautogui.keyDown('x')
+pyautogui.keyDown('3')
+pyautogui.keyDown('enter')
+    #Clearing The Terminal - Separate Program
+pyautogui.keyDown('c')
+pyautogui.keyDown('l')
+pyautogui.keyDown('e')
+pyautogui.keyDown('a')
+pyautogui.keyDown('r')
+pyautogui.keyDown('enter')
+    # Check Python Version - Separate Program
+pyautogui.keyDown('p')
+pyautogui.keyDown('y')
+pyautogui.keyDown('t')
+pyautogui.keyDown('h')
+pyautogui.keyDown('o')
+pyautogui.keyDown('n')
+pyautogui.keyDown('enter')
+
+# So Basically Using This Program You Can Automate Terminal Commands By Just Typing The Code One Time And Then After That -
+# Commands That Took 10 seconds will now take 5 seconds
+# I Know This Program Might Be Useless For A Lot Of People But I Think It's A Great Way To Practice Pyautogui And I Spend -
+# A lot of time on my terminal so that's why I made this program 
+# Enjoy The  Program !!! (:
